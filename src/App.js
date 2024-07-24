028b0aa2a06ab6b550a93edf4ceec7c4d85579c4
solomon141 <weymit2001@gmail.com>
2024-07-24 15:38:43 +0300

    api included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setPosts(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="App">
-    Bingo APP
+      <div>Bingo APP</div>
+
+      <ul>
+        {posts.length > 0 &&
+          posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        <li></li>
+      </ul>
     </div>
   );
 }
