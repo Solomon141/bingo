f5ae3a25252b9bfad7d66e10efbb720489e86225
solomon141 <weymit2001@gmail.com>
2024-07-26 15:26:30 +0300

    twa added

--- src/App.js
+++ src/App.js
@@ -4,30 +4,58 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        setPosts(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user);
+    }
   }, []);
+
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => {
+  //       setPosts(res.data);
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   return (
     <div className="App">
       <div>Bingo APP</div>
 
-      <ul>
+      {/* <ul>
         {posts.length > 0 &&
           posts.map((post) => {
             return <li key={post.id}>{post.title}</li>;
           })}
         <li></li>
-      </ul>
+      </ul> */}
+      {userData ? (
+        <>
+          <h1>User Data</h1>
+          <ul>
+            <li> {userData.id} </li>
+            <li> {userData.first_name} </li>
+            <li> {userData.last_name} </li>
+            <li> {userData.username} </li>
+            <li> {userData.language_code} </li>
+            <li>
+              {userData.is_premium ? "Premium Account" : "Ordinary Account"}
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <p> User Loading .... </p>
+        </>
+      )}
     </div>
   );
 }
